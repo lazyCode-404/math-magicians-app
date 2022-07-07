5f9e90d07bd086e61999106988ea69c58098b7ec
Bwalya Nonde <bwalyanonde8@gmail.com>
2022-07-07 17:37:51 -0400

    Add calculate.js and operate.js file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,28 +1,53 @@
-import React from 'react';
+import react from 'react';
+import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { calc: ['AC', '+/-', '%', '/', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+'] };
+class Calculator extends react.Component {
+  constructor() {
+    super();
+    this.state = {
+      total: '0',
+      next: null,
+      operation: null,
+    };
+    this.clickHandler = this.clickHandler.bind(this);
   }
 
+  clickHandler = (e) => {
+    if (!e.target.name) return;
+
+    const { next, total, operation } = calculate(this.state, e.target.name);
+
+    if (next === null && total === null) {
+      this.setState({ next, total: '0', operation });
+    } else {
+      this.setState({ next, total, operation });
+    }
+  };
+
   render() {
-    const { calc } = this.state;
+    const { total, next } = this.state;
+    const btns = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
     return (
       <div className="container">
-        <div className="screen">0</div>
+        {next ? (
+          <div className="screen">{next}</div>
+        ) : (
+          <div className="screen">{total}</div>
+        )}
         <div className="btn-column">
-
-          {calc.map((item) => (
-            <div className="btn" key={item}>
-              {item}
-            </div>
+          {btns.map((btnName) => (
+            <button
+              onClick={this.clickHandler}
+              name={btnName}
+              type="button"
+              className={`btn ${btnName === '0' ? 'btn-zero' : ''} ${
+                btnName === '=' ? 'btn-equal' : ''
+              }`}
+              key={btnName}
+            >
+              {btnName}
+            </button>
           ))}
-        </div>
-        <div className="bottom-column">
-          <div className="btn">0</div>
-          <div className="btn">.</div>
-          <div className="btn-equal">=</div>
         </div>
       </div>
     );
