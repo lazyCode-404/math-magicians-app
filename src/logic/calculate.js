aa1af75095d4d15766d212798766785a8b85c7f3
Bwalya Nonde <bwalyanonde8@gmail.com>
2022-07-08 11:20:01 -0400

    Update calculate.js

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -28,13 +28,13 @@ export default function calculate(obj, buttonName) {
     }
     // If there is an operation, update next
     if (obj.operation) {
-      if (obj.next && obj.next !== '0') {
+      if (obj.next) {
         return { ...obj, next: obj.next + buttonName };
       }
       return { ...obj, next: buttonName };
     }
     // If there is no operation, update next and clear the value
-    if (obj.next && obj.next !== '0') {
+    if (obj.next) {
       return {
         next: obj.next + buttonName,
         total: null,
@@ -54,15 +54,15 @@ export default function calculate(obj, buttonName) {
       return { ...obj, next: `${obj.next}.` };
     }
     if (obj.operation) {
-      return { ...obj, next: '0.' };
+      return { next: '0.' };
     }
     if (obj.total) {
       if (obj.total.includes('.')) {
         return {};
       }
-      return { ...obj, next: `${obj.total}.` };
+      return { total: `${obj.total}.` };
     }
-    return { ...obj, next: '0.' };
+    return { total: '0.' };
   }
 
   if (buttonName === '=') {
@@ -104,10 +104,6 @@ export default function calculate(obj, buttonName) {
   if (obj.operation) {
     if (obj.total && !obj.next) {
       return { ...obj, operation: buttonName };
-    }
-
-    if (!obj.total) {
-      return { total: 0, operation: buttonName };
     }
 
     return {
