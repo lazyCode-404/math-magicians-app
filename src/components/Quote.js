200cff4aad74f44346a5c8dfa5909e13a98e793b
Bwalya Nonde <bwalyanonde8@gmail.com>
2022-09-17 17:59:06 +0200

    Fix linters

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,5 +1,6 @@
 const Quote = () => (
   <div>
+    <h1 className="home-header">Math Magician</h1>
     <p className="home-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus a urna ac aliquam. Aliquam congue posuere ipsum sit amet efficitur. Sed odio tortor, luctus quis nisi nec, bibendum facilisis odio. Phasellus elementum purus a velit lacinia, at faucibus mauris pellentesque. Ut auctor nec justo non congue. Maecenas magna metus, pellentesque id ipsum quis, hendrerit gravida nisi.</p>
   </div>
 );
